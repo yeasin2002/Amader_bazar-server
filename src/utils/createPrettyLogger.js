45b77346a309c521d8594eccacda0ef9e5101b86
yeasin-2002 <mdkawsarislam2002@gmail.com>
2023-10-22 17:59:40 +0600

    Configured Winton Logger

--- src/utils/createPrettyLogger.js
+++ src/utils/createPrettyLogger.js
@@ -2,9 +2,17 @@ const { format, createLogger, transports } = require("winston");
 
 const createPrettyLogger = createLogger({
     level: "info",
-    format: format.combine(format.json().format.colorize(), format.simple()),
-    defaultMeta: { service: "user-service" },
-    transports: [new transports.Console()],
+    // format: format.combine(format.json().format.colorize(), format.simple()),
+    format: format.combine(
+        format.timestamp({ format: "YYYY-MM-DD HH:mm" }),
+        format.json(),
+    ),
+    transports: [
+        new transports.Console({
+            format: format.combine(format.colorize(), format.simple()),
+        }),
+        new transports.File({ filename: "./src/log/info.log", level: "info" }),
+    ],
 });
 
 module.exports = createPrettyLogger;
