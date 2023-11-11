ff02bd467c919a60533fec0e9d059353e66f995f
Md Kawsar Islam Yeasin <mdkawsarislam2002@gmail.com>
2023-11-12 00:06:26 +0600

    added automatic Script

---
+++ clear.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Delete directories and files if they exist
+[ -d "dist" ] && rm -rf dist &
+[ -d "node_modules" ] && rm -rf node_modules &
+[ -f "pnpm-lock.yaml" ] && rm pnpm-lock.yaml &
+
+# Run eslint and pnpm commands
+eslint --cache --cache-location .eslintcache --fix &
+pnpm store prune &
+
+# Wait for all operations to complete
+wait
