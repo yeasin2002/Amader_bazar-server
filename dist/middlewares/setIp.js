7c3e4bd648b3e325dc66c0261cb48a1fd7b7cb4b
Md Kawsar Islam Yeasin <mdkawsarislam2002@gmail.com>
2023-12-04 19:33:57 +0600

    fix: revert vercel config

--- dist/middlewares/setIp.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.setIp = void 0;
-const setIp = (req, res, next) => {
-    req.body.ip = req.ip;
-    next();
-};
-exports.setIp = setIp;
-//# sourceMappingURL=setIp.js.map
